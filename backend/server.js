13196c4f69cd3222733f8662b25c8aebc52fc0f5
Daniel Hernandez <daniel244@live.com.mx>
2023-02-21 14:22:51 -0600

    CRUD de user

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 app.use('/api/productos', require('./routes/productoRoutes'))
-// app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
 
